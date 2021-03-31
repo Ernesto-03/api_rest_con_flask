ccda9220fc2e2e36ce87638d8d970c11020e7617
Ernesto-03 <elopezc0308@gmail.com>
2021-03-31 00:18:30 +0000

    ultima actualizacion

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, UserFav, People, Planet, Vehicle
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,118 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+
+
 @app.route('/user', methods=['GET'])
 def handle_hello():
-
     response_body = {
         "msg": "Hello, this is your GET /user response "
     }
-
     return jsonify(response_body), 200
+
+
+
+@app.route('/userfav', methods=['GET'])
+def list_userfav():
+    userfav = UserFav.query.all()
+    response_body = {
+        "msg": "Hello, this is your GET /userfav ",
+        "data": userfav
+    }
+    return jsonify(response_body), 200
+
+
+
+@app.route('/people', methods=['GET'])
+def list_people():
+    people = People.query.all()
+    response_body = {
+        "msg": "Hello, this is your GET /people ",
+        "data": people
+    }
+    return jsonify(response_body), 200   
+
+
+
+@app.route('/planet', methods=['GET'])
+def list_planet():
+    planet = Planet.query.all()
+    response_body = {
+        "msg": "Hello, this is your GET /planet ",
+        "data": planet
+    }
+    return jsonify(response_body), 200    
+
+
+
+@app.route('/vehicle', methods=['GET'])
+def list_vehicle():
+    vehicle = Vehicle.query.all()
+    response_body = {
+        "msg": "Hello, this is your GET /vehicle ",
+        "data": vehicle
+    }
+    return jsonify(response_body), 200  
+
+
+
+@app.route('/addAll', methods=['POST'])
+def addAll():
+    body = request.get_json()
+    people = body['people']
+    planet = body['planet']
+    vehicle = body['vehicle']
+    
+    for p in people:
+        people1 = People(
+            name= p["Character Name"],
+            height = p["Character height"],
+            mass = p["Character mass"],
+            hair_color = p["Character hair color"],
+            skin_color = p["Character skin color"],
+            eye_color = p["Character eye color"],
+            birth_year = p["Character birth year"],
+            gender = p["Character gender"]
+        )
+        db.session.add(people1)
+
+    for p in planet:
+        planet1 = Planet(
+            planet_name = pl["Planet name"],
+            rotation_period = pl["rotation period"],
+            orbital_period = pl["orbital period"],
+            diameter = pl["diameter"],
+            climate = pl["climate"],
+            gravity = pl["gravity"],
+            terrain = pl["terrain"],
+            population = pl["population"]
+
+        )
+        db.session.add(planet1)
+
+    for p in vehicle:
+        vehicle1 = Vehicle(
+            vehicle_name = v["vehicle name"],
+            model = v["model"],
+            passenger = v["passenger"],
+            consumable = v["consumable"],
+            starship_class = v["starship class"],
+            lenght = v["lenght"],
+            cargo_capacity = v["cargo capacity"],
+            hyperdrive_rating = v["hyperdrive rating"]
+
+        )
+        db.session.add(vehicle1) 
+
+        
+        db.session.commit()
+    
+
+
+#user1 = Person(username="my_super_username", email="my_super@email.com")
+#db.session.add(user1)
+#db.session.commit()
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,73 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+#class User(db.Model):
+ #   id = db.db.Column(db.Integer, primary_key=True)
+  #  email = db.db.Column(db.db.String(120), unique=True, nullable=False)
+   # password = db.db.Column(db.db.String(80), unique=False, nullable=False)
+    #is_active = db.db.Column(db.Boolean(), unique=False, nullable=False)
+
+class UserFav(db.Model):
+    __tablename__ = 'userfav'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    name = db.Column(db.String(10))
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Integer, nullable=False)
+    password = db.Column(db.String(10))
+    email = db.Column(db.String(10))
+    #userfav = db.relationship(UserFav)
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    id = db.Column(db.Integer, primary_key=True)
+    planet_name = db.Column(db.String(10))
+    rotation_period = db.Column(db.Integer)
+    orbital_period = db.Column(db.Integer)
+    diameter = db.Column(db.Integer)
+    climate = db.Column(db.String(10))
+    gravity = db.Column(db.String(10))
+    terrain = db.Column(db.String(10))
+    population = db.Column(db.Integer)
+
+class People(db.Model):
+    __tablename__ = 'people'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(10))
+    height = db.Column(db.Integer)
+    mass = db.Column(db.Integer)
+    hair_color = db.Column(db.String(10))
+    skin_color = db.Column(db.String(10))
+    eye_color = db.Column(db.String(10))
+    birth_year = db.Column(db.String(10))
+    gender = db.Column(db.String(10))
+
+class Vehicle(db.Model):
+    __tablename__ = 'vehicle'
+    id = db.Column(db.Integer, primary_key=True)
+    vehicle_name = db.Column(db.String(10))
+    model = db.Column(db.Integer)
+    passenger = db.Column(db.Integer)
+    consumable = db.Column(db.String(10))
+    starship_class = db.Column(db.String(10))
+    lenght = db.Column(db.Integer)
+    cargo_capacity = db.Column(db.Integer)
+    hyperdrive_rating = db.Column(db.Integer)
+
+    
+    def to_dict(self):
+        return {}
+
+
+    #def __repr__(self):
+        #return '<User %r>' % self.username
+
+    #def serialize(self):
+        #return {
+        #    "id": self.id,
+        #    "email": self.email,
+        #   # do not serialize the password, its a security breach
+        #}
